e3832b82dd3a9cff069836b9ef891d6506a04d56
Anton Holodkov <anton.holodkov@bercut.com>
2024-02-18 16:51:20 +0300

    HW01 done

---
+++ OTUS-Homeworks/hw01_gradle/src/main/java/com/bercut/holodkov/Main.java
@@ -0,0 +1,11 @@
+package com.bercut.holodkov;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        HelloOtus helloOtus = new HelloOtus();
+        helloOtus.CallGuava();
+
+    }
+}
